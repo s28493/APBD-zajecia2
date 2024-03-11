c4eb8d4d2a18621a0154b95be222485bfed0bc18
Maksym Łopaciński <s28493@pjwstk.edu.pl>
2024-03-11 11:34:03 +0100

    Zadanie 3 na innym

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Security.AccessControl;
+
 class Program
 {
     public static void Main(string[] args)
@@ -8,5 +10,18 @@ class Program
         int zmienna = 0;
         String zmienna2 = "Modyfikacja2";
         String zmienna3 = "MODYFIKACJA3AHA";
+        int[] aha = { 1,1,2 };
+        Console.WriteLine(Zadanie3(aha));
+    }
+
+    public static double Zadanie3(int[] tablica)
+    {
+        double suma = 0;
+        foreach (int num in tablica)
+        {
+            suma += num;
+        }
+        
+        return suma / tablica.Length;
     }
 }
