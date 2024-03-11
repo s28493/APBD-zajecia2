073dcebe4a8c8704ad5c5449f11170550914e6df
Maksym Łopaciński <s28493@pjwstk.edu.pl>
2024-03-11 11:23:19 +0100

    Modifikacja 3

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -7,5 +7,6 @@ class Program
         Console.WriteLine("Hello, World!");
         int zmienna = 0;
         String zmienna2 = "Modyfikacja2";
+        String zmienna3 = "MODYFIKACJA3AHA";
     }
 }
